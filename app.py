b65bc397e522d4981ad0d86b81a4885c1c647b37
yashpauranik <107103139+yashpauranik@users.noreply.github.com>
2022-10-14 21:03:36 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -3,20 +3,35 @@ import urllib.request, json
 import requests
 import os
 app = Flask(__name__)
+def getResult():
+    def get_requestDetails():
+        requestDict = { "url" : "https://reqres.in/api/users?page=2",
+                        "method" : "get"
+                        }
+        return requestDict
+    def getRsponse():
+        requestDict = get_requestDetails()
+        if (requestDict["method"]=="get"):
+            response = requests.get(requestDict["url"])
+        else :
+            print("will implement later")
+        return response
+
+
+    def parseResponse():
+        data = getRsponse()
+        data = data.json()
+        response_list=data["data"]
+        response_dict={}
+
+        for i in response_list:
+            response_dict[i["email"]]=i
+        return (response_dict)
+    return parseResponse()
+
 
 @app.route("/")
-def get_dict():
-    url = "https://reqres.in/api/users?page=2"
-
-    #response = urllib.request.urlopen(url)
-    response = requests.get("https://reqres.in/api/users?page=2")
-    data = response.json()
-    response_list=data["data"]
-    response_dict={}
-    for i in response_list:
-        response_dict[i["email"]]=i
-
-
-    return (response_dict)
+def getData() :
+    return getResult()
 if __name__ == '__main__':
     app.run()
